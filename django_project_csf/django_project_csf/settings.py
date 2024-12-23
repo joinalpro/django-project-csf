c5bce07bd9696c8ca66c7c19f386a61a3127292d
joinalzx <joinalzx@gmail.com>
2024-12-23 15:15:05 +0600

    login profile

--- django_project_csf/django_project_csf/settings.py
+++ django_project_csf/django_project_csf/settings.py
@@ -129,3 +129,4 @@ CRISPY_TEMPLATE_PACK = "bootstrap5"
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = 'blog-home'
+LOGIN_URL = 'login'
--- django_project_csf/users/views.py
+++ django_project_csf/users/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from .forms import UserRegisterForm
-
+from django.contrib.auth.decorators import login_required
 def register(request):
     if request.method == 'POST':
         form = UserRegisterForm(request.POST)
@@ -20,5 +20,6 @@ def register(request):
 # message.warning
 # message.error
 
+@login_required
 def profile(request):
     return render(request, 'users/profile.html')

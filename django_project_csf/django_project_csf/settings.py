fd3325aa7aff925bc3ab99abfdedf589f2f929f9
joinalzx <joinalzx@gmail.com>
2024-12-26 10:03:08 +0600

    profile updated

--- django_project_csf/django_project_csf/settings.py
+++ django_project_csf/django_project_csf/settings.py
@@ -120,6 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR/'media'
+
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 CRISPY_TEMPLATE_PACK = "bootstrap5"
 

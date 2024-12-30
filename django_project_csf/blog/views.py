74d2f89ee748f9d004b821b1195d85bde2c138a8
joinalzx <joinalzx@gmail.com>
2024-12-30 17:12:37 +0600

    single post

--- django_project_csf/blog/views.py
+++ django_project_csf/blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 
@@ -8,5 +9,16 @@ def home(request):
     }  
     return render(request,'blog/home.html', context)
 
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html' # <app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
+    ordering = ['-date_post']
+    
+class PostDetailView(DetailView):
+    model = Post
+    
+    
 def about(request):
     return render(request,'blog/about.html',{'title':'about'})
+

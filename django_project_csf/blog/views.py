4de835331c1b64603c53ac5654308a49e60a8c62
joinalzx <joinalzx@gmail.com>
2024-12-31 22:23:03 +0600

    post update

--- django_project_csf/blog/views.py
+++ django_project_csf/blog/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
-from .models import Post
+from django.contrib.auth.mixins import LoginRequiredMixin,UserPassesTestMixin
+from django.views.generic import (
+    ListView, 
+    DetailView, 
+    CreateView,
+    UpdateView,
+    DeleteView,
+)
+from . models import Post
+
 
 
 def home(request):
@@ -18,6 +26,31 @@ class PostListView(ListView):
 class PostDetailView(DetailView):
     model = Post
     
+    
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+    
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+class PostUpdateView(LoginRequiredMixin,UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+    
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
     
 def about(request):
     return render(request,'blog/about.html',{'title':'about'})
